80d07833629563017523b52923301ab486d6d322
Joseph <joseph.meza@unmsm.edu.pe>
2024-04-11 07:00:01 -0500

    NavBar and Slider

---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,37 @@
+export const SERVICES = [
+  {
+    name: 'Servicios al Estudiante',
+    link: '/servicios-al-estudiante'
+  },
+  {
+    name: 'Para Empresas',
+    link: '/para-empresas'
+  },
+  {
+    name: 'Para Colegios',
+    link: '/para-colegios'
+  },
+  {
+    name: 'Egresados',
+    link: '/egresados'
+  },
+  {
+    name: 'Contáctanos',
+    link: '/contactanos'
+  }
+]
+
+export const INTRANET_LIST = [
+  {
+    name: 'Mi Cibertec-Intranet',
+    link: '/intranet'
+  },
+  {
+    name: 'Portal laboral',
+    link: '/portal-laboral'
+  },
+  {
+    name: 'Area virtual',
+    link: '/area-virtual'
+  }
+]
